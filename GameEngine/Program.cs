ac5e15fe35e85dca7e9f70f3703a2484dcc5e203
Jack Cooper <jack.cooper7776@gmail.com>
2025-07-26 00:35:01 +1000

    Add GameEngine project

---
+++ GameEngine/Program.cs
@@ -0,0 +1,38 @@
+﻿using Core.Application.Args;
+using Core.Engine;
+using Core.Engine.Proc;
+using Core.Logging;
+
+using GameEngine.Engine;
+
+namespace GameEngine
+{
+internal static class Program
+{
+    private static Logger Logger { get; } = new("BOOTSTRAP");
+
+    public static ProcessHost ProcessHost => ProcessHost.Instance;
+    public static LogManager LogManager => LogManager.Instance;
+
+    private static void Main(string[] args)
+    {
+        Logger.Log(Info.EngineInfo);
+        Logger.Log($"Start Time: {DateTime.Now}");
+
+        ProcessArgs(args);
+
+        GameProcess gameProcess = new(nameof(GameProcess));
+
+        ProcessHost.RegisterProcess(gameProcess);
+        ProcessHost.Start(TickCallback);
+    }
+
+    private static void ProcessArgs(string[] args)
+    {
+        LogLevelArgument logLevel = new();
+        logLevel.ProcessArguments(args);
+    }
+
+    private static void TickCallback() { }
+}
+}
