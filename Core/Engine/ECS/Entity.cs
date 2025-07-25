d70a117b75e493e542bf1a6ec612fe418550d2c4
Jack Cooper <jack.cooper7776@gmail.com>
2025-07-26 01:18:36 +1000

    Add basic Entity component system

--- Core/Engine/ECS/Entity.cs
+++ Core/Engine/ECS/Entity.cs
@@ -1,7 +1,30 @@
+using Core.Engine.Components;
+using Core.Logging;
+
 namespace Core.Engine
 {
-public class Entity(string name) : Object(name)
+public class Entity : Object
 {
-    
+    private readonly List<Component> _components = [];
+    private IReadOnlyList<Component> Components => _components;
+
+    public Entity(string name) : base(name)
+    {
+        Logger.Name = $"{nameof(Entity)}:{Name}";
+
+        AddComponent(new Transform(this));
+    }
+
+    public void AddComponent(Component component)
+    {
+        Logger.Log($"Adding component: {component}", LogLevel.Debug);
+        _components.Add(component);
+    }
+
+    public void RemoveComponent(Component component)
+    {
+        Logger.Log($"Removing component: {component}", LogLevel.Debug);
+        _components.Remove(component);
+    }
 }
 }
