6bf4f160b14a7e15be160144012abf1ca892b093
Jack Cooper <jack.cooper7776@gmail.com>
2025-07-26 01:19:29 +1000

    Rename Info.Name to "PabuEngine"

--- Core/Engine/Info.cs
+++ Core/Engine/Info.cs
@@ -2,7 +2,7 @@ namespace Core.Engine
 {
 public static class Info
 {
-    public const string Name = "SolarWind";
+    public const string Name = "PabuEngine";
     public const string Version = "0.0.1";
 
     public static string EngineInfo => $"{Name} - {Version}";
