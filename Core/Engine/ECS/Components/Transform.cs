0f48883e8a175c770febad186826bae76b6c7434
Jack Cooper <jack.cooper7776@gmail.com>
2025-08-01 12:02:40 +1000

    Transform default values
    Set `Transform`'s default values:
    Position: Vector3.Zero
    Rotation: `Quaternion.Identity`
    Scale: `Vector3.One`

--- Core/Engine/ECS/Components/Transform.cs
+++ Core/Engine/ECS/Components/Transform.cs
@@ -4,8 +4,8 @@ namespace Core.Engine.Components
 {
 public class Transform(Entity entity) : Component(nameof(Transform), entity)
 {
-    public Vector3 Position { get; set; }
-    public Quaternion Rotation { get; set; }
-    public Vector3 Scale { get; set; }
+    public Vector3 Position { get; set; } = Vector3.Zero;
+    public Quaternion Rotation { get; set; } = Quaternion.Identity;
+    public Vector3 Scale { get; set; } = Vector3.One;
 }
 }
